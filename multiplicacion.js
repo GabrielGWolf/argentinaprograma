dc8b03557f5b688b8036fcbe286aca5b6e851776
GABRIEL G WOLF <gabrielww20@gmail.com>
2023-05-20 11:35:10 -0300

    Subo archivos Resta y Multiplicacion

---
+++ multiplicacion.js
@@ -0,0 +1,7 @@
+let num1 = 2;
+let num2 = 17;
+let num3 = 6
+let multiplicacion = num1 * num2 * num3;
+console.log("el resultado de la multiplicación de los números 2 x 17 x 6 es " + (multiplicacion));
+
+// La multiplicación de los numeros 2*17*6 da como resultado 20
---
+++ resta.js
@@ -0,0 +1,6 @@
+let num1 = 10;
+let num2 = 2;
+let resta = num1- num2;
+console.log(resta);
+
+// La resta de 10-2 da como resultado 8
