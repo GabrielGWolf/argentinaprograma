6e4d411e62da771d7211a64f6ac3f40e5145a495
GABRIEL G WOLF <gabrielww20@gmail.com>
2023-05-20 11:38:48 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,12 @@
+let base1 = 5;
+let altura1 = 3;
+let areaRectangulo1 = base1 * altura1;
+console.log("Area de un rectangulo de 5cm de base por 3cm de altura es " + (areaRectangulo1));
+
+
+let base2 = 12.5;
+let altura2 = 5.3;
+let areaRectangulo2 = base2 * altura2;
+console.log("Area de un rectangulo de 5cm de base por 3cm de altura es " + (areaRectangulo2));
+
+// Area = Base*Altura --- en el caso de los ejercicios dados, 5cm*3cm=15cm y 12.5cm*5.3cm=66.25cm
---
+++ promedio3.js
@@ -0,0 +1,19 @@
+let num1e1 = 5;
+let num2e1 = 7;
+let num3e1 = 9;
+let promedio3e1 = (num1e1 + num2e1 + num3e1)/3;
+console.log("El promedio de 5, 7, 9 es " + (promedio3e1));
+
+let num1e2 = -2;
+let num2e2 = -3;
+let num3e2 = -8;
+let promedio3e2 = (num1e2 + num2e2 + num3e2)/3;
+console.log("El promedio de -2, -3, -8 es " + (promedio3e2));
+
+
+let num1e3 = -1.2;
+let num2e3 = 5.3;
+let num3e3 = -3.3;
+let promedio3e3 = (num1e3 + num2e3 + num3e3)/3;
+console.log("El promedio de -1.2, 5.3, -3.3 es " + (promedio3e3));
+
