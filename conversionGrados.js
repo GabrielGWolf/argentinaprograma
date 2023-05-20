9395b2b754ffe7e0ef31b8bdd4701f1b8feb4e6c
GABRIEL G WOLF <gabrielww20@gmail.com>
2023-05-20 11:35:55 -0300

    Agrego el archvio conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,11 @@
+let Farnheit1 = 50;
+let Celcius1 = (Farnheit1-32) * 5/9;
+console.log ("75°F en Celcius es " + (Celcius1));
+
+let Farnheit2 = 75;
+let Celcius2 = (Farnheit2-32) * 5/9;
+console.log ("75°F en Celcius es " + Celcius2);
+
+
+// Para 50°F,  10°Celcius
+// Para 75°F, 23.888° Celcius
