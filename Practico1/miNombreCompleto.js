1509d2a50649c8d5df243f3e08b47b9b86824177
GABRIEL G WOLF <gabrielww20@gmail.com>
2023-06-08 20:44:36 -0300

    actualice los archivos del practico 1

--- Practico1/miNombreCompleto.js
+++ Practico1/miNombreCompleto.js
@@ -2,6 +2,4 @@ let nombre;
 nombre = "Gabriel Guidon";
 let apellido = "Wolf";
 let edad = 34;
-
-//console.log("Hola, " + nombre + " " + apellido +". Tienes "+ edad +" años");
 console.log("Hola, %s %s. Tienes %i años", nombre,apellido,edad);
--- Practico1/operacionesAritmeticas.js
+++ Practico1/operacionesAritmeticas.js
@@ -6,15 +6,8 @@ let var4 = 15;
 let suma12 = var1 + var2;
 let resta43 = var4 - var3;
 
-
 let resultadofinal = suma12 * resta43;
 
 let esPar = (resultadofinal%2==0)
-
-//if esPar = true{
-//    esPar = "par" 
-//} else {
-//    esPar = "impar"
-//}
 
 console.log("Mis variables iniciales fueron %i, %i, %i y %i. La respuesta al verificar si el resultado es par es: %s", var1, var2, var3, var4, esPar)
