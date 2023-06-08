4c22f508fc2a6d8b526eb62e929766c7acb520cc
GABRIEL G WOLF <gabrielww20@gmail.com>
2023-06-08 20:59:51 -0300

    corregi frutas

--- Practico1/frutas.js
+++ Practico1/frutas.js
@@ -14,4 +14,3 @@ let saturfs = frutas.reverse();
 let primera = saturfs.pop();
 console.log("la primera fruta agregada fue " + primera);
 console.log(frutas);
-console.log("Lo primero fue crear el arreglo llamado *frutas* vacio y agregar 5 elementos, luego extraer el ultimo elemento, y finalmente extraer el primer elemento. Elegí ir imprimiendo por consola todo el proceso para poder ver como funcionaba.");
