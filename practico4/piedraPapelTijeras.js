c8d062d7d7ad910bb084f9181e96ea675820abf4
GABRIEL G WOLF <gabrielww20@gmail.com>
2023-07-01 11:50:40 -0300

    Agrego práctico 4 sin extras

---
+++ practico4/piedraPapelTijeras.js
@@ -0,0 +1,51 @@
+const readlineSync = require('readline-sync');
+
+function obtenerJugadaComputadora() {
+    let opciones = ["piedra", "papel" , "tijera"];
+    let numeroRandom = Math.floor(Math.random() * 3);
+    return opciones[numeroRandom];
+};
+let jugadaComputadora = obtenerJugadaComputadora()
+
+function obtenerJugadaUsusario() {
+    let jugada = readlineSync.question("Por favor, ingrese: piedra, papel o tijera: ")
+    jugada.toLowerCase()
+    jugada.trim()
+    if (!(jugada === "piedra" || jugada === "papel" || jugada === "tijera")) { 
+        while (!(jugada === "piedra" || jugada === "papel" || jugada === "tijera")) { 
+            jugada = readlineSync.question("La opcion ingresada no es correcta." +  
+            "Por favor, ingrese solo una palabra de las siguientes opciones: piedra, papel o tijera: ")
+            jugada = jugada.toLocaleLowerCase()
+            jugada = jugada.trim()
+            }
+        return jugada.toLocaleLowerCase()        
+        } else {
+        return jugada.toLowerCase().trim()
+    }
+};
+let jugadaUsuario = obtenerJugadaUsusario()
+
+function determinarGanador(jugadaComputadora, jugadaUsuario) {
+    if (
+      (jugadaComputadora === "piedra" && jugadaUsuario === "tijeras") ||
+      (jugadaComputadora === "tijeras" && jugadaUsuario === "papel") ||
+      (jugadaComputadora === "papel" && jugadaUsuario === "piedra")
+    ) {
+      return "Gana la computadora";
+    } else if (
+      (jugadaUsuario === "piedra" && jugadaComputadora === "tijeras") ||
+      (jugadaUsuario === "tijeras" && jugadaComputadora === "papel") ||
+      (jugadaUsuario === "papel" && jugadaComputadora === "piedra")
+    ) {
+      return "Gana el usuario";
+    } else {
+      return "Empate";
+    }
+};
+const resultado = determinarGanador(jugadaComputadora, jugadaUsuario);
+
+console.log("El usuario eligió " + jugadaUsuario)
+console.log("La computadora eligió " + jugadaComputadora);
+console.log("El resultado es: " + resultado);
+
+
