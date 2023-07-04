e0802d505a758718af013e8b42dbfc13e1281dc7
GABRIEL G WOLF <gabrielww20@gmail.com>
2023-07-03 22:42:33 -0300

    pratico 4 sin extras

--- practico4/piedraPapelTijerasExtras.js
+++ practico4/piedraPapelTijerasExtras.js
@@ -1,11 +1,13 @@
 const readlineSync = require('readline-sync');
+let veces = parseInt(readlineSync.question("Por favor, ingrese cuantas veces quiere jugar: "))
+let contador = 0;
 
 function obtenerJugadaComputadora() {
     let opciones = ["piedra", "papel" , "tijera"];
     let numeroRandom = Math.floor(Math.random() * 3);
     return opciones[numeroRandom];
 };
-let jugadaComputadora = obtenerJugadaComputadora()
+  let jugadaComputadora = obtenerJugadaComputadora()
 
 function obtenerJugadaUsusario() {
     let jugada = readlineSync.question("Por favor, ingrese: piedra, papel o tijera: ")
@@ -26,26 +28,42 @@ function obtenerJugadaUsusario() {
 let jugadaUsuario = obtenerJugadaUsusario()
 
 function determinarGanador(jugadaComputadora, jugadaUsuario) {
-    if (
-      (jugadaComputadora === "piedra" && jugadaUsuario === "tijera") ||
-      (jugadaComputadora === "tijera" && jugadaUsuario === "papel") ||
-      (jugadaComputadora === "papel" && jugadaUsuario === "piedra")
+  if (
+    (jugadaComputadora === "piedra" && jugadaUsuario === "tijera") ||
+    (jugadaComputadora === "tijera" && jugadaUsuario === "papel") ||
+    (jugadaComputadora === "papel" && jugadaUsuario === "piedra")
     ) {
-      return "Gana la computadora";
+    contador = contador--
+    return console.log("Gana la computadora")
     } else if (
-      (jugadaUsuario === "piedra" && jugadaComputadora === "tijera") ||
-      (jugadaUsuario === "tijera" && jugadaComputadora === "papel") ||
-      (jugadaUsuario === "papel" && jugadaComputadora === "piedra")
+    (jugadaUsuario === "piedra" && jugadaComputadora === "tijera") ||
+    (jugadaUsuario === "tijera" && jugadaComputadora === "papel") ||
+    (jugadaUsuario === "papel" && jugadaComputadora === "piedra")
     ) {
-      return "Gana el usuario";
+    contador = contador++
+    return "Gana el usuario";
     } else {
-      return "Empate";
+    return "Empate";
     }
-};
-const resultado = determinarGanador(jugadaComputadora, jugadaUsuario);
+  };
+  const resultado = determinarGanador(jugadaComputadora, jugadaUsuario);
 
-console.log("El usuario eligió " + jugadaUsuario)
-console.log("La computadora eligió " + jugadaComputadora);
-console.log("El resultado es: " + resultado);
+for (let i = 0; i < veces; i++) {
+  console.log("Partida " + (i + 1));
+  const jugadaComputadora = obtenerJugadaComputadora();
+  const jugadaUsuario = obtenerJugadaUsuario();
+  const resultado = determinarGanador(jugadaComputadora, jugadaUsuario);
 
+  console.log("El usuario eligió: " + jugadaUsuario);
+  console.log("La computadora eligió: " + jugadaComputadora);
+  console.log("El resultado de este partido es: " + resultado);
+  console.log("Contador actual: " + contador);
+}
 
+if (contador < 0) {
+  console.log("El ganador global es la computadora");
+} else if (contador > 0) {
+  console.log("El ganador global es el usuario");
+} else {
+  console.log("El resultado global es Empate");
+}
